7d777d3956aac7b94d92ae14d6805fec87ce11a6
Shiro Tanchiva <shiroartturoartturotanchiva@gmail.com>
2026-06-11 21:25:16 -0500

    Update sidebar-dashboard.php

--- app/views/layout/sidebar-dashboard.php
+++ app/views/layout/sidebar-dashboard.php
@@ -7,23 +7,23 @@
     <div class="p-4 text-center border-bottom border-secondary border-opacity-25">
         <h4 class="fw-bold text-white m-0">AGUA SAC PANEL</h4>
     </div>
-    <ul class="nav flex-column mt-3 flex-grow-1">
-        <li class="nav-item">
-            <a class="nav-link" href="/control-de-cobros/index.php"><i class="fa-solid fa-house"></i> Ver Landing</a>
-        </li>
-        
-        <li class="nav-item">
-            <a class="nav-link active" href="/control-de-cobros/index.php?url=clientes"><i class="fa-solid fa-users"></i> Clientes</a>
-        </li>
-        
-        <li class="nav-item">
-            <a class="nav-link" href="/control-de-cobros/index.php?url=recibos"><i class="fa-solid fa-file-invoice-dollar"></i> Recibos</a>
-        </li>
-        
-         <li class="nav-item">
-            <a class="nav-link" href="/control-de-cobros/index.php?url=pagos"><i class="fa-solid fa-money-bill-wave"></i> Pagos</a>
-        </li>
-    </ul>
+   <ul class="nav flex-column mt-3 flex-grow-1" style="height: 100vh;">
+    <li class="nav-item">
+        <a class="nav-link active" href="/control-de-cobros/index.php?url=clientes"><i class="fa-solid fa-users"></i> Clientes</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/control-de-cobros/index.php?url=recibos"><i class="fa-solid fa-file-invoice-dollar"></i> Recibos</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/control-de-cobros/index.php?url=pagos"><i class="fa-solid fa-money-bill-wave"></i> Pagos</a>
+    </li>
+
+    <li class="nav-item mt-auto mb-3">
+        <a class="nav-link text-danger" href="/control-de-cobros/index.php?url=auth/logout">
+            <i class="fa-solid fa-sign-out-alt"></i> Cerrar Sesión
+        </a>
+    </li>
+</ul>
 </div>
 <div class="d-lg-none position-fixed top-0 start-0 m-3" style="z-index: 1100;">
     <button class="btn btn-dark shadow-sm" id="toggleMenuBtn">
