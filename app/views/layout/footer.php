53103ba01e20671520cc77458e0cf0e3601bd915
Shiro Tanchiva <shiroartturoartturotanchiva@gmail.com>
2026-06-11 21:25:24 -0500

    Update footer.php

--- app/views/layout/footer.php
+++ app/views/layout/footer.php
@@ -6,6 +6,7 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="/control-de-cobros/public/js/main.js"></script>
+<script src="/control-de-cobros/public/js/clientes.js"></script>
 
 </body>
 </html>
